21acf130bb8806dff6e63bbcd6ed2deda27b2500
Nick Szapiro <nick.szapiro@noaa.gov>
2025-03-27 01:27:06 -0500

    Try nccmp args. Turn off post

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -494,11 +494,10 @@ if [[ ${skip_check_results} == false ]]; then
           if [[ " orion hercules hera wcoss2 acorn derecho gaeac5 gaeac6 jet s4 noaacloud frontera" =~ ${MACHINE_ID} ]]; then
             printf "USING NCCMP.." >> "${RT_LOG}"
             printf "USING NCCMP.."
-              if [[ ${CMP_DATAONLY} == false ]]; then
-                nccmp -d -S -q -f -g -B --Attribute=checksum --warn=format "${nccmp_exclude}" "${RTPWD}/${CNTL_DIR}_${RT_COMPILER}/${i}" "${RUNDIR}/${i}" > "${i}_nccmp.log" 2>&1 && d=$? || d=$?
-              else
-                nccmp -d -S -q -f -B --Attribute=checksum --warn=format "${nccmp_exclude}" "${RTPWD}/${CNTL_DIR}_${RT_COMPILER}/${i}" "${RUNDIR}/${i}" > "${i}_nccmp.log" 2>&1 && d=$? || d=$?
-              fi
+              args=(-d -S -q -f -B --Attribute=checksum --warn=format)
+              if [[ ${CMP_DATAONLY} == false ]];; then args+=("-g"); fi
+              if [[ ! -z "${nccmp_exclude// }" ]]; then args+=("${nccmp_exclude}"); fi
+              nccmp "${args[@]}" "${RTPWD}/${CNTL_DIR}_${RT_COMPILER}/${i}" "${RUNDIR}/${i}" > "${i}_nccmp.log" 2>&1 && d=$? || d=$?
               if [[ ${d} -ne 0 && ${d} -ne 1 ]]; then
                 printf "....ERROR" >> "${RT_LOG}"
                 printf "....ERROR"

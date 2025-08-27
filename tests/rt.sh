4d1a0af2cae46088ee4e5ec92181e5dbd8172887
dpsarmie <Daniel.Sarmiento@noaa.gov>
2025-08-26 20:16:13 -0500

    Add c1152 case

--- tests/rt.sh
+++ tests/rt.sh
@@ -870,7 +870,7 @@ case ${MACHINE_ID} in
     COMPILE_QUEUE="batch"
     PARTITION="hercules"
     dprefix=${dprefix:-"/work2/noaa/stmp/${USER}"}
-    DISKNM="/work/noaa/epic/hercules/UFS-WM_RT"
+    DISKNM="/work/noaa/stmp/dsarmien"
     STMP="${dprefix}/stmp"
     PTMP="${dprefix}/stmp"
 

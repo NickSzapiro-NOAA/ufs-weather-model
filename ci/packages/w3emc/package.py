c371ed2104bb3fd9e69d34df161558c4576b3b15
Nicholas Szapiro <149816583+NickSzapiro-NOAA@users.noreply.github.com>
2026-01-15 13:05:19 -0500

    Update git branch and hash for w3emc in ci/packages/w3emc/package.py

--- ci/packages/w3emc/package.py
+++ ci/packages/w3emc/package.py
@@ -4,7 +4,7 @@ from spack.pkg.builtin.w3emc import W3emc as BuiltinW3emc
 class W3emc(BuiltinW3emc):
     # Method A: Git Checkout (Recommended)
     # Spack clones the repo and checks out this specific commit.
-    git = "https://github.com/AlexanderRichert-NOAA/NCEPLIBS-w3emc"
+    git = "https://github.com/NOAA-EMC/NCEPLIBS-w3emc"
     
     # You only need the commit hash here:
-    version("add_module", commit="035a2a2e3356ad469c68c9edf659f89416417fe2")
+    version("module-test", commit="d923949dc98e85f440698c3a79b57aff6302fec6")

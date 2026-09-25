a1f2a4f8eea1b78523eac197bf64aa00070efc91
Nick Szapiro <nick.szapiro@noaa.gov>
2026-09-25 18:30:52 +0000

    Update CDEPS for datm_ufs

--- CDEPS-interface/cdeps_files.cmake
+++ CDEPS-interface/cdeps_files.cmake
@@ -50,6 +50,7 @@ list(APPEND cdeps_datm_files
   CDEPS/datm/datm_datamode_cplhist_mod.F90
   CDEPS/datm/datm_datamode_era5_mod.F90
   CDEPS/datm/datm_datamode_gefs_mod.F90
+  CDEPS/datm/datm_datamode_ufs_mod.F90
   CDEPS/datm/datm_datamode_jra_mod.F90
   CDEPS/datm/datm_datamode_simple_mod.F90
   CDEPS/datm/datm_pres_aero_mod.F90
